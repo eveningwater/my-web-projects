452351de7b493f188ef7aadc33b49cdd4fadc34b
夕水 <854806732@qq.com>
2020-07-12 17:47:21 +0800

    优化了音乐播放器示例

--- js/10/index.js
+++ js/10/index.js
@@ -18,7 +18,9 @@ const progress = $('#progress');
 const audio = $("#audio");
 const openMusicList = $("#open-music-list");
 const musicList = $("#music-list");
-
+const musicTime = $("#music-time");
+const mode = $("#mode");
+const child = mode.querySelector('.icon');
 const songs = [
     {
         name: "她不是真的爱我",
@@ -74,14 +76,90 @@ const songs = [
         name: "绵阳印象",
         singer: "羽上轩",
         imgName: "yushangxuan"
+    },
+    {
+        name: "滚蛋歌",
+        singer: "张翰",
+        imgName: "zhanghan"
+    },
+    {
+        name: "烈火青春",
+        singer: "张雨生",
+        imgName: "zhangyusheng"
+    },
+    {
+        name: "成都",
+        singer: "赵雷",
+        imgName: "zhaolei"
+    },
+    {
+        name: "歌唱二小放牛郎",
+        singer: "杨慧妍",
+        imgName: "yanghuiyan"
+    },
+    {
+        name: "我的唇吻不到我爱的人",
+        singer: "王奕心",
+        imgName: "wangyixin"
+    },
+    {
+        name: "贝多芬的悲伤",
+        singer: "萧风",
+        imgName: "xiaofeng"
+    },
+    {
+        name: "菊花爆满山",
+        singer: "马博",
+        imgName: "mabo"
+    },
+    {
+        name: "一亿个伤心",
+        singer: "蒙面哥",
+        imgName: "mengmiange"
+    },
+    {
+        name: "帝都",
+        singer: "萌萌哒天团",
+        imgName: "mengmengdatiantuan"
+    },
+    {
+        name: "陪你一起变老",
+        singer: "唐古",
+        imgName: "tanggu"
+    },
+    {
+        name: "我在前世约了你",
+        singer: "唐古",
+        imgName: "tanggu"
+    },
+    {
+        name: "恋人心",
+        singer: "魏新雨",
+        imgName: "weixinyu"
+    },
+    {
+        name: "新娘不是我",
+        singer: "程响",
+        imgName: "chengxiang"
     }
 ];
+let modeCount = 0;
 console.log('歌曲数:',songs.length);
-// 随机取音乐
-let songIndex = Math.floor(Math.random() * songs.length);
+//音乐索引值
+let songIndex = 0;
 // console.log(songIndex);
-loadSong(songs[songIndex], songIndex);
 loadSongList();
+/**
+ * 设置音乐时间
+ * @param {*} time 
+ */
+function setMusicTime(time){
+    musicTime.innerHTML = '';
+    let minute = Math.floor(time / 60),second = Math.floor(time % 60);
+    if(minute < 10)minute = '0' + minute;
+    if(second < 10)second = '0' + second;
+    musicTime.innerHTML = `${ minute } : ${ second }`;
+}
 /**
  * 加载音乐列表
  */
@@ -143,28 +221,6 @@ function pauseSong() {
     playBtn.querySelector('.fas').classList.add('fa-play');
     audio.pause();
 }
-/**
- * 播放上一首
- */
-function prevSong() {
-    songIndex--;
-    if (songIndex < 0) {
-        songIndex = songs.length - 1;
-    }
-    loadSong(songs[songIndex], songIndex);
-    playSong();
-}
-/**
- * 播放下一首
- */
-function nextSong() {
-    songIndex++;
-    if (songIndex > songs.length - 1) {
-        songIndex = 0;
-    }
-    loadSong(songs[songIndex], songIndex);
-    playSong();
-}
 // 点击播放或暂停
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -184,6 +240,7 @@ function updateProgress(e) {
     // console.log(duration,currentTime);
     const percent = (currentTime / duration) * 100;
     progress.style.width = percent + '%';
+    setMusicTime(currentTime);
 }
 /**
  * 设置进度条
@@ -198,14 +255,70 @@ function setProgress(e) {
     audio.currentTime = time;
 }
 // 点击上一首
-prevBtn.addEventListener('click', prevSong);
+prevBtn.addEventListener('click', () => {
+    songIndex = selectSongByMode(child.className);
+    loadSong(songs[songIndex],songIndex);
+    playSong();
+});
 // 点击下一首
-nextBtn.addEventListener('click', nextSong);
+nextBtn.addEventListener('click', () => {
+    songIndex = selectSongByMode(child.className,true);
+    loadSong(songs[songIndex],songIndex);
+    playSong();
+});
 // 歌曲播放完时自动播放下一首
-audio.addEventListener('ended', nextSong);
+audio.addEventListener('ended', () => {
+    songIndex = selectSongByMode(child.className);
+    loadSong(songs[songIndex],songIndex);
+    playSong();
+});
 // 歌曲播放时更新进度条
 audio.addEventListener('timeupdate', updateProgress);
 // 设置进度条
 progressContainer.addEventListener('click', setProgress);
 // 打开音乐列表
 openMusicList.addEventListener('click',onOpenMusicList);
+mode.addEventListener('click',() => {
+    modeCount++;
+    const className = child.className.slice(child.className.indexOf('-') - 6);
+    // console.log(className);
+    setMode(className);
+});
+/**
+ * 设置模式
+ * @param {*} className 
+ * @param {*} isNext 
+ */
+function setMode(className){
+    // 等于1为单曲循环，2为随机播放，0为顺序播放
+    if(modeCount % 3 === 1){
+        child.classList.replace(className,'repeat-once');
+    }else if(modeCount % 3 === 2){
+        child.classList.replace(className,'repeat-random');
+    }else{
+        child.classList.replace(className,'repeat-cycle');
+    }
+}
+/**
+ * 选择歌曲
+ * @param {*} mode 
+ * @param {*} isNext 
+ */
+function selectSongByMode(mode,isNext){
+    if(mode.indexOf('once') > -1){
+        return songIndex;
+    }else if(mode.indexOf('random') > -1){
+        return songIndex = Math.floor(Math.random() * songs.length);
+    }else{
+        if(isNext){
+            songIndex++;
+            if (songIndex > songs.length - 1) {
+                songIndex = 0;
+            }
+        }else{
+            songIndex--;
+            if(songIndex < 0)songIndex = songs.length - 1;
+        }
+        return songIndex;
+    }
+}
