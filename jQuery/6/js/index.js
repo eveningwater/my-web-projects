1b34375a804688eadf1e45eb1880aff32dc8d3a5
夕水 <854806732@qq.com>
2020-07-14 00:32:02 +0800

    优化了留言评论示例功能

--- jQuery/6/js/index.js
+++ jQuery/6/js/index.js
@@ -1,7 +1,7 @@
 class Talk {
     constructor() {
         this.flag = false;
-        this.recommon = $('.recommon');
+        this.commentBtn = $('.recommon');
         this.editImage = $('.editor_img');
         this.editorDiv = $('.editor_bqdiv');
         this.editorText = $('.editor_text');
@@ -9,19 +9,18 @@ class Talk {
     /**
      * 初始化页面功能
      */
-    init(){
-        this.initRecommon(this.recommon);
-        this.initEditImage(this.editImage,this.editorDiv,this.editorText);
+    init() {
+        this.initComment(this.commentBtn);
+        this.initEditImage(this.editImage, this.editorDiv, this.editorText);
     }
     /**
      * 点击表情
      * @param {*} editImageButton 
      * @param {*} editorDiv 
-     * @param {*} editorText 
      */
-    initEditImage(editImageButton,editorDiv,editorText){
+    initEditImage(editImageButton, editorDiv) {
         const _this = this;
-        editImageButton.click(function(){
+        editImageButton.click(function () {
             _this.flag = !_this.flag;
             if (_this.flag) {
                 editorDiv.slideDown(100);
@@ -40,13 +39,13 @@ class Talk {
                     td.eq(k).append(`<img src='img/tsj${k + 1}.gif' alt='图片加载中'/>`);
                     td.eq(k).click(function () {
                         const idx = $(this).index();
-                        editorText.append(`<img src='img/tsj${idx + 1}.gif' alt='图片加载中'/>`);
+                        _this.insertHtmlAtCaret(`<img src='img/tsj${idx + 1}.gif' alt='图片加载中'/>`);
                         editorDiv.slideUp(100);
                         _this.flag = false;
                         editorDiv.html('');
                     });
                 }
-        
+
             } else {
                 editorDiv.slideUp(100);
                 editorDiv.html('');
@@ -55,22 +54,22 @@ class Talk {
     }
     /**
      * 点击留言
-     * @param {*} recommonButton 
+     * @param {*} commentBtn 
      */
-    initRecommon(recommonButton) {
+    initComment(commentBtn) {
         const _this = this;
-        recommonButton.click(function () {
+        commentBtn.click(function () {
             $('body').append(`<div class="commonPanel"><i class='logo'></i><div class='headerTool'><span class='logoName'></span><span class='floor'></span><span class='date'></span></div><p></p></div>`);
-            var commonpanel = $('.commonPanel p'),
-                len = commonpanel.length;
+            var commonPanel = $('.commonPanel p'),
+                len = commonPanel.length;
             var html = $('.editor_text').html();
             $('.logoName').eq(len - 1).text('游客');
             $('.floor').eq(len - 1).html('第' + len + '楼');
             $('.date').eq(len - 1).html(_this.nowDate());
             if (len == 1) {
-                commonpanel.eq(0).html(html);
+                commonPanel.eq(0).html(html);
             } else {
-                commonpanel.eq(len - 1).html(html);
+                commonPanel.eq(len - 1).html(html);
             }
         });
     }
@@ -79,7 +78,7 @@ class Talk {
      * @param {*} time 
      */
     nowDate(time) {
-        let formatNumber = function(n){
+        let formatNumber = function (n) {
             n = n.toString();
             return n[1] ? n : "0" + n;
         }
@@ -90,6 +89,42 @@ class Talk {
             hour = date.getHours(),
             minutes = date.getMinutes();
         return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minutes].map(formatNumber).join(':');
+    }
+    /**
+     * 往光标处添加
+     * @param {*} html 
+     */
+    insertHtmlAtCaret(html) {
+        var sel, range;
+        if (window.getSelection) {
+            // IE9 and non-IE
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+                // Range.createContextualFragment() would be useful here but is
+                // non-standard and not supported in all browsers (IE9, for one)
+                var el = document.createElement("div");
+                el.innerHTML = html;
+                var frag = document.createDocumentFragment(),
+                    node, lastNode;
+                while ((node = el.firstChild)) {
+                    lastNode = frag.appendChild(node);
+                }
+                range.insertNode(frag);
+                // Preserve the selection
+                if (lastNode) {
+                    range = range.cloneRange();
+                    range.setStartAfter(lastNode);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            }
+        } else if (document.selection && document.selection.type !== "Control") {
+            // IE < 9
+            document.selection.createRange().pasteHTML(html);
+        }
     }
 }
 new Talk().init();
