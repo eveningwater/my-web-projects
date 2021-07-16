ad33549032d0849a1bc55c86ed3663abe4ff35fd
eveningwater <854806732@qq.com>
2021-07-16 15:54:15 +0800

    修改了代码

--- js/83/script.js
+++ js/83/script.js
@@ -1,13 +1,40 @@
-const leftSlide = document.querySelector(".left-slide");
-const rightSlide = document.querySelector(".right-slide");
-const arrowDown = document.querySelector(".arrow-down-btn");
-const arrowUp = document.querySelector(".arrow-up-btn");
-const l = leftSlide.querySelectorAll(".left-slide-item").length;
-let currentIndex = 0;
-let slideContainer = document.querySelector(".slide-container");
-let position = window.innerHeight;
+const $ = v => document.querySelector(v);
+const slideContainer = $(".slide-container");
+const leftSlide = $(".left-slide");
+const rightSlide = $(".right-slide");
+const arrowDown = $(".arrow-down-btn");
+const arrowUp = $(".arrow-up-btn");
+const slideLeftItem = leftSlide.querySelectorAll(".left-slide-item");
+const slideRightItem = rightSlide.querySelectorAll(".right-slide-item");
+const l = slideLeftItem.length;
+let currentIndex = 0,width = window.innerWidth,position;
 function setPosition(el){
-    el.style.top = "-" + (l - 1) * 100 +"vh";
+    let itemWidth,slideLeftWidth,slideRightWidth,left,top;
+    if(isHorizontal()){
+        itemWidth = width + 'px';
+        slideLeftWidth = slideRightWidth = width * l + 'px';
+        left = -(width * (l - 1)) + 'px';
+        top = 0;
+    }else{
+        itemWidth = "100%";
+        slideLeftWidth = "35%";
+        slideRightWidth = "65%";
+        left = 0;
+        top = "-" + (l - 1) * 100 + "vh";
+    }
+    [...slideLeftItem,...slideRightItem].forEach(item => item.style.width = itemWidth);
+    leftSlide.style.width= slideLeftWidth;
+    rightSlide.style.width = slideRightWidth;
+    [
+        {
+            prop:"left",
+            value:left
+        },
+        {
+            prop:"top",
+            value:top
+        }
+    ].forEach(item => el.style[item.prop] = item.value);
 }
 function changeSlide(direction){
     if(direction === 'up'){
@@ -23,14 +50,24 @@ function changeSlide(direction){
     }
     setTransform();
 }
+function isHorizontal(){
+    return width <= 869;
+}
 function setTransform(){
-    leftSlide.style.transform = "translateY(" + (position * currentIndex) +"px)";
-    rightSlide.style.transform = "translateY(" + -(position * currentIndex) +"px)";
+    let translate = isHorizontal() ? "translateX" : "translateY";
+    leftSlide.style.transform = `${ translate }(${position * currentIndex}px)`;
+    rightSlide.style.transform = `${ translate }(${-(position * currentIndex)}px)`;
+}
+function setPositionValue(){
+    position = isHorizontal() ? width : window.innerHeight
 }
 window.onload = () => {
+    setPositionValue();
     setPosition(leftSlide);
     window.addEventListener("resize",() => {
-        position = window.innerHeight;
+        width = window.innerWidth;
+        setPositionValue();
+        setPosition(leftSlide);
         setTransform();
     })
     arrowDown.addEventListener("click",() => changeSlide("down"));
