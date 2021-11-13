a9142220fccc2094c8a2fbf22b10aca155b182c4
eveningwater <854806732@qq.com>
2021-11-13 17:34:52 +0800

    优化了代码

--- 50-mini-web-website/vite.config.ts
+++ 50-mini-web-website/vite.config.ts
@@ -18,6 +18,15 @@ export default defineConfig({
      port:8081
   },
   build:{
-   assetsInlineLimit:0
+   assetsInlineLimit:0,
+   cssTarget:"chrome61",
+   /**
+    * This options allows users to set a different browser target for CSS minification from the one used for JavaScript transpilation.
+      It should only be used when you are targeting a non-mainstream browser. 
+      One example is Android WeChat WebView, which supports most modern JavaScript features but not the #RGBA hexadecimal color 
+      notation in CSS.
+       In this case, you need to set build.cssTarget to `chrome61` to prevent vite from transform rgba() colors into #RGBA 
+       hexadecimal notations.Take a look at the doc:https://vitejs.dev/config/#build-csstarget
+    */
   }
 })
