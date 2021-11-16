47f6c745f99d3cf004c86568b88e575e05a7a9b3
eveningwater <854806732@qq.com>
2021-11-16 21:07:10 +0800

    添加了新的vue示例

---
+++ vue/41/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+  @import "./styles/variable.scss";
+  body {
+    .app {
+        font-family:$font-family;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+  }
+</style>
