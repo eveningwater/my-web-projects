53ededc99c09e2f6eeea99ae3ffce73602373d15
eveningwater <854806732@qq.com>
2022-03-28 11:38:08 +0800

    新写了读取文件列表的php接口

--- js/26/js/index.js
+++ js/26/js/index.js
@@ -1,12 +1,15 @@
 //页面加载时调用封装函数
 window.onload = function () {
     let xhr = new XMLHttpRequest();
-    xhr.open("get","data/data.json",true);
+    xhr.open("get","https://www.eveningwater.com/my-web-projects/js/26/readFileList.php",true);
     xhr.send();
     xhr.onreadystatechange = () => {
         if(xhr.readyState === 4 && xhr.status === 200){
-            const data = JSON.parse(xhr.responseText);
-            new Gallery({ data });
+            const res = JSON.parse(xhr.responseText);
+            if(res.code === 200){
+                const { data } = res;
+                new Gallery({ data });
+            }
         }
     }
 }
