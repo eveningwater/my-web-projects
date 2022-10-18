c893cb7c83e1a656af5273362ef3e89625e25289
eveningwater <854806732@qq.com>
2022-10-18 20:37:48 +0800

    fix: 修改了连连看游戏

--- vue/81/src/core/gameConfig.ts
+++ vue/81/src/core/gameConfig.ts
@@ -1,5 +1,5 @@
-export const BASE_IMAGE_URL = "https://www.eveningwater.com/my-web-projects/js/26/img/";
-export const materialList: Record<string,string> [] = new Array(12).fill(null).map((item,index) => ({ title:`图片-${index + 1}`,src:`${BASE_IMAGE_URL + (index + 1)}.jpg`}));
+export const BASE_IMAGE_URL = "https://www.eveningwater.com/img/segmentfault/";
+export const materialList: Record<string,string> [] = new Array(12).fill(null).map((item,index) => ({ title:`思否猫-${index + 1}`,src:`${BASE_IMAGE_URL + (index + 1)}.png`}));
 export const defaultConfig: GlobalModule.GameConfigType = {
     materialList,
     time: 120,
