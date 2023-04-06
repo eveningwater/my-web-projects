cda96c4ffab0839b15f3893edf71a4a5bd0df288
eveningwater <854806732@qq.com>
2023-04-06 10:20:09 +0800

    feat: 添加了vue-axios示例

---
+++ vue/82/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { get } from './axios';
+
+get('https://jsonplaceholder.typicode.com/todos/1').then(data => {
+    console.log(1111,data)
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/82/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
+import 'element-plus/dist/index.css';
+createApp(App).mount('#app');
