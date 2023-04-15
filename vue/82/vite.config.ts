3f579e699687ffa672ea1c61c8e5e915ac37a85f
eveningwater <854806732@qq.com>
2023-04-15 21:56:21 +0800

    fix: 修改了vue示例的打包路径

--- vue/82/vite.config.ts
+++ vue/82/vite.config.ts
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
