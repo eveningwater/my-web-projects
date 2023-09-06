90a58745fdaae7b206c1076e258a088ccc5ff671
eveningwater <854806732@qq.com>
2023-09-06 21:37:09 +0800

    feat: 修改了提示

--- js/114/scripts/index.js
+++ js/114/scripts/index.js
@@ -3,8 +3,8 @@ class Elevator {
     this.count = count;
     this.onFloor = 1;
     this.btnGroup = null;
-    this.zoneContainer = this.$(".ew-elevator-storey-zone");
-    this.elevator = this.$(".ew-elevator");
+    this.zoneContainer = this.$('.ew-elevator-storey-zone');
+    this.elevator = this.$('.ew-elevator');
     this.generateStorey(this.count || 6);
     this.doorCloseTime = 3 * 1000;
   }
@@ -15,39 +15,41 @@ class Elevator {
     return el.querySelectorAll(selector);
   }
   generateStorey(count) {
-    let template = "";
+    let template = '';
     for (let i = count - 1; i >= 0; i--) {
       template += `
                 <div class="ew-elevator-storey">
                     <div class="ew-elevator-controller">
-                        <button type="button" class="ew-elevator-to-top ew-elevator-btn" ${i === count - 1 ? "disabled" : ""
-        }>↑</button>
-                        <button type="button" class="ew-elevator-to-bottom ew-elevator-btn" ${i === 0 ? "disabled" : ""
-        }>↓</button>
+                        <button type="button" class="ew-elevator-to-top ew-elevator-btn" ${
+                          i === count - 1 ? 'disabled' : ''
+                        }>↑</button>
+                        <button type="button" class="ew-elevator-to-bottom ew-elevator-btn" ${
+                          i === 0 ? 'disabled' : ''
+                        }>↓</button>
                     </div>
                     <div class="ew-elevator-count">${i + 1}</div>
                 </div>
             `;
     }
     this.zoneContainer.innerHTML = template;
-    this.storeys = this.$$(".ew-elevator-storey", this.zoneContainer);
-    this.doors = this.$$(".ew-elevator-door", this.elevator);
-    this.btnGroup = this.$$(".ew-elevator-btn", this.zoneContainer);
+    this.storeys = this.$$('.ew-elevator-storey', this.zoneContainer);
+    this.doors = this.$$('.ew-elevator-door', this.elevator);
+    this.btnGroup = this.$$('.ew-elevator-btn', this.zoneContainer);
     [...this.storeys].forEach((item, index) => {
       this.handleClick(
-        this.$$(".ew-elevator-btn", item),
+        this.$$('.ew-elevator-btn', item),
         item.offsetHeight,
         index
       );
     });
   }
   handleClick(btnGroup, floorHeight, floorIndex) {
-    Array.from(btnGroup).forEach((btn) => {
-      btn.addEventListener("click", () => {
-        if (btn.classList.contains("checked")) {
+    Array.from(btnGroup).forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (btn.classList.contains('checked')) {
           return;
         }
-        btn.classList.add("checked");
+        btn.classList.add('checked');
         const currentFloor = this.count - floorIndex;
         const moveFloor = currentFloor - 1;
         this.elevatorMove(currentFloor, floorHeight * moveFloor);
@@ -59,30 +61,33 @@ class Elevator {
     const diffFloor = Math.abs(num - currentFloor);
 
     this.addStyles(this.elevator, {
-      transitionDuration: diffFloor + "s",
-      bottom: offset + "px",
+      transitionDuration: diffFloor + 's',
+      bottom: offset + 'px'
     });
 
     // wait for elevator move to target floor
     setTimeout(() => {
       [...this.btnGroup].forEach((btn, index) => {
-
         // open door
-        Array.from(this.doors).forEach((door) => door.classList.add("toggle"));
+        Array.from(this.doors).forEach(door => door.classList.add('toggle'));
 
-        if(!this.$('.ew-message')){
-          $message.success(
-            `本美女就要出来了，请速速来迎接,再等${this.doorCloseTime / 1000
-            }s就关电梯门了!`
-          );
+        if (!this.$('.ew-message')) {
+          ewMessage.success({
+            content: `本美女就要出来了，请速速来迎接,再等${
+              this.doorCloseTime / 1000
+            }s就关电梯门了!`,
+            duration: 3000
+          });
         }
 
         // close door
         setTimeout(() => {
-          Array.from(this.doors).forEach((door) => door.classList.remove("toggle"));
+          Array.from(this.doors).forEach(door =>
+            door.classList.remove('toggle')
+          );
         }, this.doorCloseTime);
 
-        btn.classList.remove("checked");
+        btn.classList.remove('checked');
       });
     }, diffFloor * 1000);
 
@@ -92,4 +97,3 @@ class Elevator {
     Object.assign(el.style, styles);
   }
 }
-
