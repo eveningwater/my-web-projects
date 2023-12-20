7ca22aa9bfc7d6c02337579d3e62c9af3ebecd20
eveningwater <854806732@qq.com>
2023-12-20 23:15:48 +0800

    feat: 修改了返回首页的跳转链接

--- vue/85/src/components/Error/Error.vue
+++ vue/85/src/components/Error/Error.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import { INDEX_URL } from '../../const';
 import { errorIcon } from '../../const/icon';
 import { computed } from 'vue';
 const getErrorHTML = computed(() => `
     ${errorIcon('ew-note-error-icon')}
     <p>暂未找到该页面!</p>
-    <a href="/"  class="ew-note-error-link">返回首页</a>
+    <a href="${INDEX_URL}"  class="ew-note-error-link">返回首页</a>
 `);
 </script>
 <template>
---
+++ vue/85/src/const/index.ts
@@ -0,0 +1,1 @@
+export const INDEX_URL = process.env.NODE_ENV !== 'production' ? '/' : 'https://www.eveningwater.com/my-web-projects/vue/85/#/'
