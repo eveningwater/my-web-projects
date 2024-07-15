2ae28a9db8f158694845e2702980fb271392984f
eveningwater <eveningwater.123@bytedance.com>
2024-07-15 14:44:37 +0800

    feat: 修改了代码

--- react/57/src/App.tsx
+++ react/57/src/App.tsx
@@ -1,38 +1,38 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import './style/app.scss';
-import $message from './utils/message';
-import { translate,$ } from './utils/util';
+import { useState } from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import "./style/app.scss";
+import $message from "ew-message";
+import { translate, $ } from "./utils/util";
 
 function App() {
-  const [lang,setLang] = useState("en");
-  const [word,setWord] = useState("");
-  const onChangeHandler = (v:string) => setLang(v);
-  const onChangeWordHandler = (v:string) => setWord(v);
-  const onClickHandler = (type:string) => {
-      if(type === "clear"){
-        setWord("");
+  const [lang, setLang] = useState("en");
+  const [word, setWord] = useState("");
+  const onChangeHandler = (v: string) => setLang(v);
+  const onChangeWordHandler = (v: string) => setWord(v);
+  const onClickHandler = (type: string) => {
+    if (type === "clear") {
+      setWord("");
+    }
+    if (type === "translate") {
+      if (!word) {
+        return $message.warning("请输入需要翻译的单词或语句!");
       }
-      if(type === "translate"){
-          if(!word){
-             return $message.warning("请输入需要翻译的单词或语句!");
-          }
-          const s = $("#translateScript");
-          if (s) {
-            (s.parentElement as HTMLElement).removeChild(s);
-          }
-          translate(word, lang);
+      const s = $("#translateScript");
+      if (s) {
+        (s.parentElement as HTMLElement).removeChild(s);
       }
-  }
+      translate(word, lang);
+    }
+  };
   return (
     <div className="app">
-       <main className="rt-main">
-           <Header onChange={onChangeHandler}></Header>
-           <Content onChangeWord={onChangeWordHandler} propWord={word}></Content>
-           <Footer onClick={onClickHandler}></Footer>
-       </main>
+      <main className="rt-main">
+        <Header onChange={onChangeHandler}></Header>
+        <Content onChangeWord={onChangeWordHandler} propWord={word}></Content>
+        <Footer onClick={onClickHandler}></Footer>
+      </main>
     </div>
   );
 }
--- react/57/src/react-app-env.d.ts
+++ react/57/src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+type ResSuccessType = {
+  trans_result: Array<{ dst: string; src: string }>;
+  from: string;
+  to: string;
+};
+type ResErrorType = {
+  error_code: string;
+  error_msg: string;
+};
+type ResType = ResSuccessType | ResErrorType;
+declare interface Window {
+  getTransLateCallback?: (v: ResType) => void;
+}
+interface PropType {
+  onChangeWord: Function;
+  propWord: string;
+}
